f1d068d392372dc7f7784998f2ca2d4bb0a149b9
Deepak <deepakn@mit.edu>
2014-11-28 17:06:21 -0500

    Fixed FileStreamReader, and added simple query using stream-to-relator converters -- still need to add output checking

--- src/java/simpledb/FileStreamReader.java
+++ src/java/simpledb/FileStreamReader.java
@@ -2,40 +2,72 @@ package simpledb;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class FileStreamReader implements StreamReader {
 	
 	private ArrayList<Tuple> tuples;
 	private int currentIndex;
+	private TupleDesc td;
+	private HashMap<Integer, Integer> startTimes;
+	private HashMap<Integer, Integer> endTimes;
+	private boolean returnNullNext = false;
 	
-	public FileStreamReader(String filename, TupleDesc td) throws Exception {
+	public FileStreamReader(String filename, TupleDesc td) throws IOException {
 		this.tuples = new ArrayList<Tuple>();
+		startTimes = new HashMap<Integer, Integer> ();
+		endTimes = new HashMap<Integer, Integer> ();
 		
 		BufferedReader br = new BufferedReader(new FileReader(filename));
 		String line;
+		int lastStartTimeRecorded = -1;
+		Tuple lastTuple = null;
+		int cnt = 0;
 		while ((line = br.readLine()) != null) {
-			this.tuples.add(makeTuple(line, td));
+			Tuple newTuple = makeTuple(line, td);
+			tuples.add(newTuple);
+			TSField tsField = (TSField) newTuple.getField(newTuple.getTupleDesc().numFields() - 1);
+			if (tsField.getValue() > lastStartTimeRecorded) {
+				startTimes.put(tsField.getValue(), cnt);
+				lastStartTimeRecorded = tsField.getValue();
+				if (lastTuple != null) {
+					TSField lasttsField = (TSField) lastTuple.getField(lastTuple.getTupleDesc().numFields() - 1);
+					endTimes.put(lasttsField.getValue(), cnt - 1);
+				}
+			}
+			lastTuple = newTuple;
+			cnt++;
+		}
+		if (lastTuple != null) {
+			TSField lasttsField = (TSField) lastTuple.getField(lastTuple.getTupleDesc().numFields() - 1);
+			endTimes.put(lasttsField.getValue(), cnt - 1);
 		}
 		br.close();
 		
 		currentIndex = 0;
+		this.td = td;
 	}
 
 	@Override
 	public Tuple getNext(int ts) {
-		Tuple currentTuple = tuples.get(currentIndex);
-		TSField tsField = (TSField) currentTuple.getField(currentTuple.getTupleDesc().numFields() - 1);
-		
-		if (tsField.getValue() == ts) {
-			this.currentIndex += 1;
-			return currentTuple;
+		if (returnNullNext) {
+			returnNullNext = false;
+			return null;
 		}
-		
-		return null;
+		if (!(currentIndex >= startTimes.get(ts) && currentIndex <= endTimes.get(ts))) {
+			currentIndex = startTimes.get(ts);
+		}
+		if (currentIndex == endTimes.get(ts)) {
+			returnNullNext = true;
+		}
+		Tuple currentTuple = tuples.get(currentIndex);
+		currentIndex++;
+		return currentTuple;
 	}
 	
-	private Tuple makeTuple(String str, TupleDesc td) throws Exception {
+	private Tuple makeTuple(String str, TupleDesc td) {
 		String[] splitted = str.split("\\s+");
 		
 		Tuple tuple = new Tuple(td);
@@ -51,7 +83,7 @@ public class FileStreamReader implements StreamReader {
 				tuple.setField(i, new TSField(Integer.parseInt(splitted[i])));
 				continue;
 			} else {
-				throw new Exception("Type not supported for file reading");
+				throw new RuntimeException("Type not supported for file reading");
 			}
 		}
 		
@@ -60,8 +92,7 @@ public class FileStreamReader implements StreamReader {
 
 	@Override
 	public TupleDesc getTupleDesc() {
-		// TODO: Implement this
-		return null;
+		return td;
 	}
 
 }
--- test/simplecql/StreamToRelationTimeWindowConverterTest.java
+++ test/simplecql/StreamToRelationTimeWindowConverterTest.java
@@ -2,19 +2,32 @@ package simplecql;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.NoSuchElementException;
 
 import org.junit.Test;
 
+import simpledb.Aggregate;
 import simpledb.DbException;
 import simpledb.DbIterator;
+import simpledb.FileStreamReader;
+import simpledb.Filter;
 import simpledb.IntField;
+import simpledb.Operator;
+import simpledb.Predicate;
+import simpledb.Predicate.Op;
+import simpledb.Aggregator;
 import simpledb.SimpleStreamReader;
 import simpledb.Stream;
+import simpledb.StreamReader;
 import simpledb.StreamToRelationTimeWindowConverter;
 import simpledb.TransactionAbortedException;
 import simpledb.Tuple;
+import simpledb.TupleDesc;
+import simpledb.TupleIterator;
+import simpledb.Type;
 
 public class StreamToRelationTimeWindowConverterTest {
 	
@@ -45,7 +58,7 @@ public class StreamToRelationTimeWindowConverterTest {
 		SimpleStreamTestHelper(10);
 	}
 
-	public void SimpleStreamTestHelper(int windowSize)
+	private void SimpleStreamTestHelper(int windowSize)
 			throws NoSuchElementException, DbException, TransactionAbortedException {
 		SimpleStreamReader sr = new SimpleStreamReader();
 		Stream stream = new Stream(sr);
@@ -89,6 +102,54 @@ public class StreamToRelationTimeWindowConverterTest {
 				}
 			}
 			iterator.close();
+		}
+	}
+	
+	private DbIterator applyOperator(TupleDesc td, Operator operator) throws DbException, TransactionAbortedException {
+		ArrayList<Tuple> output = new ArrayList<Tuple> ();
+		operator.open();
+		while (operator.hasNext()) {
+			output.add(operator.next());
+		}
+		operator.close();
+		return new TupleIterator(td, output);
+	}
+	
+	private DbIterator mergeIterators(TupleDesc td, DbIterator iterator1, DbIterator iterator2) throws DbException, TransactionAbortedException {
+		ArrayList<Tuple> merge = new ArrayList<Tuple> ();
+		if (iterator1 != null) {
+			iterator1.open();
+			while (iterator1.hasNext()) {
+				merge.add(iterator1.next());
+			}
+			iterator1.close();
+		}
+		iterator2.open();
+		while (iterator2.hasNext()) {
+			merge.add(iterator2.next());
+		}
+		iterator2.close();
+		return new TupleIterator(td, merge);
+	}
+	
+	@Test
+	public void FileStreamWithAggregationTest() throws IOException, DbException, TransactionAbortedException {
+		TupleDesc td = new TupleDesc(new Type[]{Type.INT_TYPE, Type.INT_TYPE, Type.TS_TYPE});
+		StreamReader sr = new FileStreamReader("aggregation_test.txt", td);
+		
+		Stream stream = new Stream(sr);
+		StreamToRelationTimeWindowConverter converter = new StreamToRelationTimeWindowConverter(stream, 0, td);
+		DbIterator merge = null;
+		// aggregation_test has data for 50 timesteps
+		for (int i = 0; i < 50; i++) {
+			converter.updateRelation();
+			DbIterator input = converter.getRelation();
+			Operator filter = new Filter(new Predicate(1, Op.GREATER_THAN_OR_EQ, new IntField(20)), input);
+			DbIterator intermediate = applyOperator(td, filter);
+			Operator aggregate = new Aggregate(intermediate, 1, 2, Aggregator.Op.AVG);
+			TupleDesc aggregateDesc = new TupleDesc(new Type[]{Type.TS_TYPE, Type.INT_TYPE});
+			DbIterator output = applyOperator(aggregateDesc, aggregate);
+			merge = mergeIterators(aggregateDesc, merge, output);
 		}
 	}
 
