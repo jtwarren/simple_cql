54793181b4650977dac1ed953c8c6ac9cb1da768
Deepak <deepakn@mit.edu>
2014-11-30 20:46:15 -0500

    Removed unneeded import

--- src/java/simpledb/RelationToRstreamConverter.java
+++ src/java/simpledb/RelationToRstreamConverter.java
@@ -1,13 +1,9 @@
 package simpledb;
 
-import java.util.ArrayList;
-
 public class RelationToRstreamConverter implements RelationToStreamConverter {
-    private TupleDesc td;
     private RelationStreamReader reader;
 
     public RelationToRstreamConverter(TupleDesc td) {
-        this.td = td;
         reader = new RelationStreamReader(td);
     }
     
