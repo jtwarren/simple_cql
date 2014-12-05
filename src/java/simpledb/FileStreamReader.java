d9564becbbdb521adec6dcea8ed74a6826cd9b6e
Deepak <deepakn@mit.edu>
2014-12-05 16:39:44 -0500

    Fixed FileStreamReader

--- src/java/simpledb/FileStreamReader.java
+++ src/java/simpledb/FileStreamReader.java
@@ -5,66 +5,48 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class FileStreamReader implements StreamReader {
 	
-	private ArrayList<Tuple> tuples;
-	private int currentIndex;
+	private Iterator<Tuple> iterator;
+	private int ts;
+
 	private TupleDesc td;
-	private HashMap<Integer, Integer> startTimes;
-	private HashMap<Integer, Integer> endTimes;
-	private boolean returnNullNext = false;
+	private HashMap<Integer, ArrayList<Tuple>> tuples;
 	
 	public FileStreamReader(String filename, TupleDesc td) throws IOException {
-		this.tuples = new ArrayList<Tuple>();
-		startTimes = new HashMap<Integer, Integer> ();
-		endTimes = new HashMap<Integer, Integer> ();
+		this.tuples = new HashMap<Integer, ArrayList<Tuple>> ();
 		
 		BufferedReader br = new BufferedReader(new FileReader(filename));
 		String line;
-		int lastStartTimeRecorded = -1;
-		Tuple lastTuple = null;
-		int lastTs = -1;
-		int cnt = 0;
 		while ((line = br.readLine()) != null) {
 			Tuple newTuple = makeTuple(line, td);
 			int newTs = getTs(line);
-			tuples.add(newTuple);
-			if (newTs > lastStartTimeRecorded) {
-				startTimes.put(newTs, cnt);
-				lastStartTimeRecorded = newTs;
-				if (lastTuple != null) {
-					endTimes.put(lastTs, cnt - 1);
-				}
-			}
-			lastTuple = newTuple;
-			lastTs = newTs;
-			cnt++;
-		}
-		if (lastTuple != null) {
-			endTimes.put(lastTs, cnt - 1);
+			if (!tuples.containsKey(newTs))
+				tuples.put(newTs, new ArrayList<Tuple> ());
+			tuples.get(newTs).add(newTuple);
 		}
 		br.close();
 		
-		currentIndex = 0;
+		ts = -1;
 		this.td = td;
 	}
 
 	@Override
 	public Tuple getNext(int ts) {
-		if (returnNullNext) {
-			returnNullNext = false;
+		if (this.ts != ts) {
+			this.ts = ts;
+			if (!tuples.containsKey(ts)) {
+				return null;
+			}
+			iterator = tuples.get(ts).iterator();
+		}
+		if (!iterator.hasNext()) {
+			this.ts = -1;
 			return null;
 		}
-		if (!(currentIndex >= startTimes.get(ts) && currentIndex <= endTimes.get(ts))) {
-			currentIndex = startTimes.get(ts);
-		}
-		if (currentIndex == endTimes.get(ts)) {
-			returnNullNext = true;
-		}
-		Tuple currentTuple = tuples.get(currentIndex);
-		currentIndex++;
-		return currentTuple;
+		return iterator.next();
 	}
 	
 	private Tuple makeTuple(String str, TupleDesc td) {
