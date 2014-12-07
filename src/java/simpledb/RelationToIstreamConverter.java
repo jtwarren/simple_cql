ffe6aad7ea11435d040db9bb21fdb7eb56b0e80b
nauttuan <tanguyen@mit.edu>
2014-12-07 13:07:20 -0500

    patched up the relation-->Istream converter to handle both cases of record id

--- src/java/simpledb/RelationToIstreamConverter.java
+++ src/java/simpledb/RelationToIstreamConverter.java
@@ -21,23 +21,34 @@ public class RelationToIstreamConverter implements RelationToStreamConverter {
     }
     
     public void updateStream(DbIterator nextRelation) throws DbException, TransactionAbortedException {
+        HashSet<Integer> joinDiff = new HashSet<Integer>();
         HashSet<Tuple> diff = new HashSet<Tuple>();
         Tuple prevTuple;
         if (prevRelation != null) { // only diff with prev TS if existed
             prevRelation.rewind();
             while (prevRelation.hasNext()) {
                 prevTuple = prevRelation.next();
-                diff.add(prevTuple);
+                if (prevTuple.getRecordId() != null) {
+                    joinDiff.add(prevTuple.getRecordId().tupleno());
+                }
+                else {
+                    diff.add(prevTuple);
+                }
             }
         }
 
         ArrayList<Tuple> iStreamNew = new ArrayList<Tuple>();
-        nextRelation.open();;
+        nextRelation.open();
         ArrayList<Tuple> tuples = new ArrayList<Tuple> ();
         while (nextRelation.hasNext()) {
             Tuple nextTuple = nextRelation.next();
             tuples.add(nextTuple);
-            if (diff.contains(nextTuple)) {
+            if (nextTuple.getRecordId() != null) {
+                if (joinDiff.contains(nextTuple.getRecordId().tupleno())) {
+                    continue;
+                }
+            }
+            else if (diff.contains(nextTuple)) {
                 continue;
             }
             iStreamNew.add(nextTuple);
