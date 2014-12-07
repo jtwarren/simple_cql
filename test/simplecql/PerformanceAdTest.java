87114efb3b46b279230451e1c88ede2a2df031e4
Deepak <deepakn@mit.edu>
2014-12-07 14:28:33 -0500

    Naive implementations

--- test/simplecql/PerformanceAdTest.java
+++ test/simplecql/PerformanceAdTest.java
@@ -1,23 +1,100 @@
 package simplecql;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import org.junit.Test;
 
+import simpledb.DbException;
 import simpledb.DbIterator;
 import simpledb.FileStreamReader;
+import simpledb.Filter;
+import simpledb.IntField;
 import simpledb.Join;
 import simpledb.JoinPredicate;
+import simpledb.Operator;
 import simpledb.Predicate;
 import simpledb.RelationToIstreamConverter;
 import simpledb.Stream;
 import simpledb.StreamReader;
 import simpledb.StreamToRelationTimeWindowConverter;
+import simpledb.TransactionAbortedException;
+import simpledb.Tuple;
 import simpledb.TupleDesc;
+import simpledb.TupleIterator;
 import simpledb.Type;
+import simpledb.Predicate.Op;
 
 public class PerformanceAdTest {
+	
+	@Test
+	public void timeWindowSystemTestNaive() throws IOException, DbException, TransactionAbortedException {
+		long timestart = System.currentTimeMillis();
+		TupleDesc itd = new TupleDesc(new Type[] { Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE });
+		TupleDesc nitd = new TupleDesc(new Type[] { Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE });
+		TupleDesc etd = new TupleDesc(new Type[] { Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE });
+		TupleDesc netd = new TupleDesc(new Type[] { Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE });
+		TupleDesc jtd = new TupleDesc(new Type[] { Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE });
+
+		StreamReader isr = new FileStreamReader("ad_insert.txt", itd);
+		StreamReader est = new FileStreamReader("ad_event.txt", etd);
+
+		Stream insertStream = new Stream(isr);
+		Stream eventStream = new Stream(est);
+
+		ArrayList<Tuple> allInsertTuples = new ArrayList<Tuple> ();
+		ArrayList<Tuple> allEventTuples = new ArrayList<Tuple> ();
+    	long timemid = System.currentTimeMillis();
+		for (int i = 0; i < 1800; i++) {
+			Tuple tuple;
+			tuple = insertStream.getNext(i);
+			while (tuple != null) {
+				Tuple newTuple = new Tuple(nitd);
+				for (int j = 0; j < 4; j++) {
+					newTuple.setField(j, tuple.getField(j));
+				}
+				newTuple.setField(4, new IntField(i));
+				allInsertTuples.add(newTuple);
+				tuple = insertStream.getNext(i);
+			}
+			tuple = eventStream.getNext(i);
+			while (tuple != null) {
+				Tuple newTuple = new Tuple(netd);
+				for (int j = 0; j < 3; j++) {
+					newTuple.setField(j, tuple.getField(j));
+				}
+				newTuple.setField(3, new IntField(i));
+				allEventTuples.add(newTuple);
+				tuple = eventStream.getNext(i);
+			}
+			
+			Operator filter1 = new Filter(
+					new Predicate(4, Op.GREATER_THAN_OR_EQ, new IntField(i - 600)),
+					new TupleIterator(nitd, allInsertTuples));
+			DbIterator insertions = Utility.applyOperator(nitd, filter1);
+
+			Operator filter2 = new Filter(
+					new Predicate(3, Op.GREATER_THAN_OR_EQ, new IntField(i)),
+					new TupleIterator(netd, allEventTuples));
+			DbIterator events = Utility.applyOperator(netd, filter2);			
+			
+			JoinPredicate p = new JoinPredicate(0, Predicate.Op.EQUALS, 1);
+	        Join joinOp = new Join(p, insertions, events);
+	        
+	        DbIterator output = Utility.applyOperator(jtd, joinOp);
+			
+		}
+		
+		long timeend = System.currentTimeMillis();
+		
+		System.out.println("Metrics for Naive implementation");
+    	System.out.println(timemid / 1000 - timestart / 1000);
+    	System.out.println(timeend / 1000 - timemid / 1000);
+    	System.out.println("");
+	}
 
 	@Test
-	public void timeWindowSystemTest() throws Exception {
+	public void timeWindowSystemTest() throws IOException, DbException, TransactionAbortedException {
 		long timestart = System.currentTimeMillis();
 		TupleDesc itd = new TupleDesc(new Type[] { Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE });
 		TupleDesc etd = new TupleDesc(new Type[] { Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE });
@@ -50,18 +127,10 @@ public class PerformanceAdTest {
 		
 		long timeend = System.currentTimeMillis();
 		
+		System.out.println("Metrics for windowing implementation");
     	System.out.println(timemid / 1000 - timestart / 1000);
     	System.out.println(timeend / 1000 - timemid / 1000);
-    	
-//		Stream outputStream = rToSConverter.getStream();
-		
-//		for (int ts = 0; ts < 600; ts++) {
-//			Tuple tuple = outputStream.getNext(ts);
-//			while (tuple != null) {
-//				System.out.println(tuple);
-//				tuple = outputStream.getNext(ts);
-//			}
-//		}
+    	System.out.println("");
 
 	}
 
--- test/simplecql/PerformanceErrorTest.java
+++ test/simplecql/PerformanceErrorTest.java
@@ -1,9 +1,13 @@
 package simplecql;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import simpledb.Aggregate;
 import simpledb.Aggregator;
+import simpledb.DbException;
 import simpledb.DbIterator;
 import simpledb.FileStreamReader;
 import simpledb.Filter;
@@ -17,15 +21,62 @@ import simpledb.Stream;
 import simpledb.StreamReader;
 import simpledb.StreamToRelationTimeWindowConverter;
 import simpledb.StringField;
+import simpledb.TransactionAbortedException;
 import simpledb.Tuple;
 import simpledb.TupleDesc;
+import simpledb.TupleIterator;
 import simpledb.Type;
 import simpledb.Predicate.Op;
 
 public class PerformanceErrorTest {
+	
+	@Test
+	public void timeWindowSystemTestNaive() throws IOException, DbException, TransactionAbortedException {
+		long timestart = System.currentTimeMillis();
+		TupleDesc td = new TupleDesc(new Type[] { Type.STRING_TYPE });
+		TupleDesc ntd = new TupleDesc(new Type[] { Type.STRING_TYPE, Type.INT_TYPE });
+		TupleDesc otd = new TupleDesc(new Type[] { Type.INT_TYPE });
+		
+		StreamReader isr = new FileStreamReader("error_log.txt", td);
+		Stream logStream = new Stream(isr);
+		
+		ArrayList<Tuple> allTuples = new ArrayList<Tuple> ();
+		long timemid = System.currentTimeMillis();
+		for (int i = 0; i < 1800; i++) {
+			Tuple tuple = logStream.getNext(i);
+			while (tuple != null) {
+				Tuple newTuple = new Tuple(ntd);
+				newTuple.setField(0, tuple.getField(0));
+				newTuple.setField(1, new IntField(i));
+				allTuples.add(newTuple);
+				tuple = logStream.getNext(i);
+			}
+			
+			Operator filter1 = new Filter(
+					new Predicate(1, Op.GREATER_THAN_OR_EQ, new IntField(i - 2)),
+					new TupleIterator(ntd, allTuples));
+			DbIterator intermediate1 = Utility.applyOperator(ntd, filter1);
+			
+			Operator filter2 = new Filter(
+					new Predicate(0, Op.EQUALS, new StringField("server_500", Type.STRING_LEN)),
+					intermediate1);
+			DbIterator intermediate2 = Utility.applyOperator(ntd, filter2);
+			
+			Operator aggregate = new Aggregate(intermediate2, 0, -1, Aggregator.Op.COUNT);
+			
+			DbIterator output = Utility.applyOperator(otd, aggregate);
+		}
+		
+		long timeend = System.currentTimeMillis();
+		
+		System.out.println("Metrics for Naive implementation");
+		System.out.println(timemid / 1000 - timestart / 1000);
+    	System.out.println(timeend / 1000 - timemid / 1000);
+    	System.out.println("");
+	}
 
 	@Test
-	public void timeWindowSystemTest() throws Exception {
+	public void timeWindowSystemTest() throws IOException, DbException, TransactionAbortedException {
 		long timestart = System.currentTimeMillis();
 		TupleDesc td = new TupleDesc(new Type[] { Type.STRING_TYPE });
 		TupleDesc otd = new TupleDesc(new Type[] { Type.INT_TYPE });
@@ -34,8 +85,7 @@ public class PerformanceErrorTest {
 
 		Stream logStream = new Stream(isr);
 		
-		StreamToRelationTimeWindowConverter insertStreamConverter = new StreamToRelationTimeWindowConverter(logStream, 5, td);
-		
+		StreamToRelationTimeWindowConverter insertStreamConverter = new StreamToRelationTimeWindowConverter(logStream, 2, td);
 		RelationToIstreamConverter rToSConverter = new RelationToIstreamConverter(otd);
 
     	long timemid = System.currentTimeMillis();
@@ -55,8 +105,10 @@ public class PerformanceErrorTest {
 		
 		long timeend = System.currentTimeMillis();
 		
+		System.out.println("Metrics for windowing implementation");
     	System.out.println(timemid / 1000 - timestart / 1000);
     	System.out.println(timeend / 1000 - timemid / 1000);
+    	System.out.println("");
 
 	}
 }
