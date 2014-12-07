ed4f95bf64b347f5514efd0a867d42611733f9bd
Deepak <deepakn@mit.edu>
2014-12-06 21:11:45 -0500

    One more graph, indicating that garbage collection affects performance adversely

--- scripts/graph.py
+++ scripts/graph.py
@@ -17,13 +17,13 @@ def plot_times1(times1, times2):
     plt.plot(times1, label="No garbage collection")
     plt.plot(times2, label="Garbage collection")
     plt.xlabel('Discrete time value')
-    plt.ylabel('Memory footprint (in MB)')
-    plt.axis([0, 1000, 0, 100])
+    plt.ylabel('Total time (in ms) per discrete timestamp value')
+    plt.axis([0, 1000, 400, 600])
     plt.legend()
     plt.show()
 
 
 if __name__ == '__main__':
-    times1 = process_file('../outputFiles/noGC_memoryFootprint.txt')
-    times2 = process_file('../outputFiles/GC_memoryFootprint.txt')
+    times1 = process_file('../outputFiles/noGC_highInputThroughput.txt')
+    times2 = process_file('../outputFiles/GC_highInputThroughput.txt')
     plot_times1(times1, times2)
