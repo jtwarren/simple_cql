cc8b320093756a77c01b64c4eac29478607fbea4
Jeffrey Warren <jtwarren@mit.edu>
2014-12-07 22:40:17 -0500

    runner for tredning

--- runner.sh
+++ runner.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rps=(12000 13000 14000 15000 16000 17000 18000 19000 20000)
+rps=(1000 2000 3000 4000 5000 6000 7000 8000 9000 10000)
 
 for r in "${rps[@]}"
 do
 
-echo `cd scripts; python error_data.py $r | grep "qwerty" 2>&1 | tee -a ../wheee.txt; cd -; ant runcqltest -Dtest=PerformanceErrorTest | grep "qwerty" 2>&1 | tee -a wheee.txt`
+echo `cd scripts; python trending_data.py $r | grep "qwerty" 2>&1 | tee -a ../trending_test.txt; cd -; ant runcqltest -Dtest=PerformanceTrendingTest | grep "qwerty" 2>&1 | tee -a trending_test.txt`
 
 done
